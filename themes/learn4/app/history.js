8e1f927071f8c5f9cb7cd92db431862c12b17ff5
tituspijean <tituspijean@outlook.com>
2021-06-29 22:52:55 +0200

    Do not include Learn4 directory
    We have it already listed as a dependency of YunoHost-doc theme
    Will solve our multiple "Bump..." PRs too

--- themes/learn4/app/history.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-// History
-$(document).on('click', '[data-clear-history-toggle]', (event) => {
-    event.preventDefault();
-
-    window.sessionStorage.clear();
-    window.location.reload();
-});
