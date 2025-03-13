0d2daf8aba38e1d5f00ff3307383bbe63abb9bc2
Félix Piédallu <felix@piedallu.me>
2025-03-13 09:36:23 +0100

    cleanup theme code

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,15 +1,16 @@
 import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import the FontAwesomeIcon component.
-import { library } from '@fortawesome/fontawesome-svg-core'; // Import the library component.
-import { fab } from '@fortawesome/free-brands-svg-icons'; // Import all brands icons.
-import { fas } from '@fortawesome/free-solid-svg-icons'; // Import all solid icons.
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
-library.add(fab, fas); // Add all icons to the library so you can use them without importing them individually.
+// Add all icons to the library so you can use them without importing them individually.
+library.add(fab, fas);
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  FAIcon: FontAwesomeIcon, // Make the FontAwesomeIcon component available in MDX as <icon />.
+  FAIcon: FontAwesomeIcon,
 };
