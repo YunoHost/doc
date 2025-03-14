c0a6ead978deb42f7dca17b63d7e38df21d55380
Félix Piédallu <felix@piedallu.me>
2025-03-14 17:09:13 +0100

    Add columns and react-player

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -6,11 +6,16 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
+import Columns from '@site/src/components/Columns';
+import Column from '@site/src/components/Column';
+
 // Add all icons to the library so you can use them without importing them individually.
 library.add(fab, fas);
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
+  Columns,
+  Column,
   FAIcon: FontAwesomeIcon,
 };
