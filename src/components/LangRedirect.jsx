6484695ccba28cc39ba260470e64d13f54f12c8c
Félix Piédallu <felix@piedallu.me>
2025-03-23 15:57:38 +0100

    Create HardRedirects and DocumentedHardRedirect

--- src/components/LangRedirect.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {useDoc} from '@docusaurus/plugin-content-docs/client';
-import HardRedirect from '@site/src/components/HardRedirect';
-
-export default function LangRedirect({lang}) {
-  const location = useDoc().metadata.id;
-  const newLocation = `/${lang}/${location}`;
-  return <HardRedirect url={newLocation}/>
-}
