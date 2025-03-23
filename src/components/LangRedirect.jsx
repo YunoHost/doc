c3bdbe349e9f2f39a1068ad69e209f01ed54812b
Félix Piédallu <felix@piedallu.me>
2025-03-23 15:32:52 +0100

    Fix LangRedirect, create HardRedirect

--- src/components/LangRedirect.jsx
+++ src/components/LangRedirect.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Redirect, useLocation} from '@docusaurus/router';
+import {useDoc} from '@docusaurus/plugin-content-docs/client';
+import HardRedirect from '@site/src/components/HardRedirect';
 
-export function LangRedirect({lang}) {
-  const location = useLocation();
-  const newLocation = "/" + lang + location.pathname;
-  return <Redirect to={newLocation} />;
+export default function LangRedirect({lang}) {
+  const location = useDoc().metadata.id;
+  const newLocation = `/${lang}/${location}`;
+  return <HardRedirect url={newLocation}/>
 }
