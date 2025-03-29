7c0a7abdf3c6adb9be0925baa212f05d81935032
Félix Piédallu <felix@piedallu.me>
2025-03-29 18:35:45 +0100

    move images around

--- static/img/images_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while IFS= read -r file
-do
-    # echo $file
-    # [ -f "$file" ] && echo "$file does not exist"
-    if [ ! -f $file ]; then
-        echo "https://yunohost.org/images/"$file
-    fi
-done < "images.list"
