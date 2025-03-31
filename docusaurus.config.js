8415e872b09cc7eee68e136a01b1c4e4ad2731fb
Félix Piédallu <felix.piedallu@non.se.com>
2025-03-31 11:33:56 +0200

    Fix image for open graph protocol

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,7 +90,7 @@ const config = {
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'https://yunohost.org/assets/img/portal_simple_dark.jpg',
     docs: {
       sidebar: {
         autoCollapseCategories: true,
