4413ad8e2097b43ae02d01ddf58134978579656a
Félix Piédallu <felix.piedallu@non.se.com>
2025-04-02 11:02:10 +0200

    generate_docs: docs directories were renamed

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -8,7 +8,7 @@ SRCDIR=$1
 
 DOCDIR=$(dirname "$SCRIPT_DIR")
 
-"$SCRIPT_DIR/helpers_doc_generate.py" 2     -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/10.packaging_apps/20.scripts/20.helpers_v2.0.mdx"
-"$SCRIPT_DIR/helpers_doc_generate.py" 2.1   -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/10.packaging_apps/20.scripts/20.helpers_v2.1.mdx"
-"$SCRIPT_DIR/resources_doc_generate.py"     -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/10.packaging_apps/10.manifest/10.app_resources.mdx"
-"$SCRIPT_DIR/forms_doc_generate.py"         -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/15.dev/forms.mdx"
+"$SCRIPT_DIR/helpers_doc_generate.py" 2     -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/10.apps/20.scripts/20.helpers_v2.0.mdx"
+"$SCRIPT_DIR/helpers_doc_generate.py" 2.1   -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/10.apps/20.scripts/20.helpers_v2.1.mdx"
+"$SCRIPT_DIR/resources_doc_generate.py"     -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/10.apps/10.manifest/10.app_resources.mdx"
+"$SCRIPT_DIR/forms_doc_generate.py"         -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/15.core/forms.mdx"
