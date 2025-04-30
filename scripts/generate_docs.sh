467c9cd8bf387e9597d02f1b1ca020cc1e808def
Félix Piédallu <felix.piedallu@non.se.com>
2025-04-30 09:44:03 +0200

    Update scripts/generate_docs.sh after moving pages around

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -8,12 +8,12 @@ SRCDIR=$1
 
 DOCDIR=$(dirname "$SCRIPT_DIR")
 
-"$SCRIPT_DIR/helpers_doc_generate.py" 2     -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/10.apps/20.scripts/20.helpers_v2.0.mdx"
-"$SCRIPT_DIR/helpers_doc_generate.py" 2.1   -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/10.apps/20.scripts/20.helpers_v2.1.mdx"
-"$SCRIPT_DIR/resources_doc_generate.py"     -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/10.apps/10.manifest/10.resources.mdx"
-"$SCRIPT_DIR/forms_doc_generate.py"         -i "$SRCDIR" -o "$DOCDIR/docs/06.contribute/15.core/forms.mdx"
+"$SCRIPT_DIR/helpers_doc_generate.py" 2     -i "$SRCDIR" -o "$DOCDIR/docs/packaging/20.scripts/20.helpers_v2.0.mdx"
+"$SCRIPT_DIR/helpers_doc_generate.py" 2.1   -i "$SRCDIR" -o "$DOCDIR/docs/packaging/20.scripts/20.helpers_v2.1.mdx"
+"$SCRIPT_DIR/resources_doc_generate.py"     -i "$SRCDIR" -o "$DOCDIR/docs/packaging/10.manifest/10.resources.mdx"
+"$SCRIPT_DIR/forms_doc_generate.py"         -i "$SRCDIR" -o "$DOCDIR/docs/dev/15.core/forms.mdx"
 
 # Fixups until the sources are updated
-sed -i 's|/packaging_config_panels|/contribute/apps/advanced/config_panels|' "$DOCDIR/docs/06.contribute/15.core/forms.mdx"
-sed -i 's|#read-and-write-values-the|/contribute/apps/advanced/config_panels#the-bind-statement|' "$DOCDIR/docs/06.contribute/15.core/forms.mdx"
-sed -i 's|(#actions)|(/contribute/apps/advanced/config_panels#actions)|' "$DOCDIR/docs/06.contribute/15.core/forms.mdx"
+sed -i 's|/packaging_config_panels|/contribute/apps/advanced/config_panels|' "$DOCDIR/docs/dev/15.core/forms.mdx"
+sed -i 's|#read-and-write-values-the|/contribute/apps/advanced/config_panels#the-bind-statement|' "$DOCDIR/docs/dev/15.core/forms.mdx"
+sed -i 's|(#actions)|(/contribute/apps/advanced/config_panels#actions)|' "$DOCDIR/docs/dev/15.core/forms.mdx"
