00b1a5fe9dca93c5350bbe0c60150750fa4b042b
Félix Piédallu <felix@piedallu.me>
2025-05-08 15:00:17 +0200

    forms.mdx were patched for the new docs urls in the yunohost repo

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -12,8 +12,3 @@ DOCDIR=$(dirname "$SCRIPT_DIR")
 "$SCRIPT_DIR/helpers_doc_generate.py" 2.1   -i "$SRCDIR" -o "$DOCDIR/docs/packaging/20.scripts/20.helpers_v2.1.mdx"
 "$SCRIPT_DIR/resources_doc_generate.py"     -i "$SRCDIR" -o "$DOCDIR/docs/packaging/10.manifest/10.resources.mdx"
 "$SCRIPT_DIR/forms_doc_generate.py"         -i "$SRCDIR" -o "$DOCDIR/docs/dev/15.core/forms.mdx"
-
-# Fixups until the sources are updated
-sed -i 's|/packaging_config_panels|/packaging/advanced/config_panels|' "$DOCDIR/docs/dev/15.core/forms.mdx"
-sed -i 's|#read-and-write-values-the|/packaging/advanced/config_panels#the-bind-statement|' "$DOCDIR/docs/dev/15.core/forms.mdx"
-sed -i 's|(#actions)|(/packaging/advanced/config_panels#actions)|' "$DOCDIR/docs/dev/15.core/forms.mdx"
