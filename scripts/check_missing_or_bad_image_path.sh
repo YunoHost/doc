817571f8d26b61711ce7998781fd3e9e28103992
Alexandre Aubin <alex.aubin@mailoo.org>
2025-06-23 03:39:05 +0200

    Fix broken image srcs, add a script that checks that all img src do exist

---
+++ scripts/check_missing_or_bad_image_path.sh
@@ -0,0 +1,18 @@
+#!/bin/Bash
+#
+cd -- "$(dirname $( dirname -- "${BASH_SOURCE[0]}" ) )"
+
+RET=0
+
+for FILE in $(grep -hro "/img[^)\"'?]*" ./docs/ ./i18n/*/docusaurus-plugin-content-docs/current/ | sort | uniq)
+do 
+    if ! test -e ./static$FILE
+    then
+        echo "Image $FILE does not exists but is used as src in some file ?"
+        echo "------------"
+        grep -nr "$FILE" ./docs/ ./i18n/*/docusaurus-plugin-content-docs/current/
+        RET=1
+    fi
+done
+
+exit $RET
