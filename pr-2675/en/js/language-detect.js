c54d7a6cce629acd08a2b62370bb6424806a99d0
alexAubin <alexAubin@users.noreply.github.com>
2025-11-09 19:07:38 +0000

    deploy: 0dde2ca530be849374b5eade58c7316e2e3b8682

---
+++ pr-2675/en/js/language-detect.js
@@ -0,0 +1,52 @@
+/*
+Most of this file is taken from https://github.com/lullaby6/language-redirect under the MIT license (many thanks to them!)
+ */
+
+window.languageRedirect = redirects => {
+    redirects = Object.keys(redirects).reduce((acc, key) => {
+        acc[key.toLowerCase()] = redirects[key];
+        return acc;
+    }, {});
+
+    const currentPath = window.location.pathname;
+
+    // if the doc is already on a locale path, do not redirect
+    if (Object.keys(redirects).some(locale => currentPath.startsWith(`/${locale}/`))) {
+        return;
+    }
+
+    const data = {
+        user_language: navigator.language || navigator.userLanguage,
+        short_language: (navigator.language || navigator.userLanguage).split('-')[0]
+    }
+
+    Object.values(data).forEach(language => {
+        const redirect = redirects[language]
+
+        if (
+            !redirect ||
+            redirect === window.location.pathname ||
+            `/${redirect}` === window.location.pathname ||
+            redirect === window.location.href
+        ) return
+
+        redirectnoslash = redirect.replace(/\/$/, "");
+        window.location.replace(`${redirectnoslash}${window.location.pathname}`)
+    })
+
+    return data
+}
+
+
+if (
+    document &&
+    document.currentScript &&
+    document.currentScript.src &&
+    document.currentScript.dataset.languageRedirect
+) {
+    try {
+        window.languageRedirect(JSON.parse(document.currentScript.dataset.languageRedirect))
+    } catch (error) {
+        console.error(error)
+    }
+}
