2bffe5d7e3097e5c11d557e855eaf37b58bcf160
Alexandre Aubin <alex.aubin@mailoo.org>
2025-11-19 04:17:16 +0100

    Zgrompf⁴?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 import {Redirect} from '@docusaurus/router';
 export default function Home() {
-    return <Redirect to="/admin" />;
+    return <Redirect to="./admin" />;
 };
