0b16bd750951b23e0a54dcc4304e4307f4d8548c
Félix Piédallu <felix@piedallu.me>
2026-09-03 22:31:21 +0200

    scripts: write_all_translations: Call the proper node package manager
    Use the standard env var npm_execpath

--- scripts/write_all_translations.sh
+++ scripts/write_all_translations.sh
@@ -4,7 +4,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 set -x
 
+# Might be yarn, bun etc
+executor=${npm_execpath:-npm}
+
 langs=$(grep "enabled_locales =" $SCRIPT_DIR/../docusaurus.config.ts | awk -F= '{print $2}' | tr -d '[]",;')
 for lang in $langs; do
-    ./node_modules/@docusaurus/core/bin/docusaurus.mjs write-translations -l "$lang"
+    "$executor" run docusaurus write-translations -l "$lang" "$@"
 done
